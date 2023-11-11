be90f3dc00c90e23193b5693050ee32ecbe77366
mateomckee <mateomckee64@gmail.com>
2023-11-11 03:15:46 -0600

    Fixed function.json

--- src/CallSearchFunction/index.js
+++ src/CallSearchFunction/index.js
@@ -3,41 +3,47 @@ const { DefaultAzureCredential } = require('@azure/identity');
 const { SecretClient } = require('@azure/keyvault-secrets');
 
 module.exports = async function (context, req) {
-  try {
-    const keyVaultName = 'mentormolesecrets';
-    const secretName = 'BingSearchAPIKey1';
+    console.log('function running');
+    try {
+        const keyVaultName = 'mentormolesecrets';
+        const secretName = 'BingSearchAPIKey1';
 
-    //get the API key from Azure Key Vault using Managed Identity
-    const credential = new DefaultAzureCredential();
-    const secretClient = new SecretClient(`https://${keyVaultName}.vault.azure.net`, credential);
-    const apiKey = await secretClient.getSecret(secretName);
-    //use the retrieved Bing API key
-    const query = req.query.q || '';
-    const response = await axios.get('https://api.bing.microsoft.com/bing/v7.0/videos/search', {
-      params: { q: query },
-      headers: { 'Ocp-Apim-Subscription-Key': apiKey.value },
-    });
+        //get the API key from Azure Key Vault using Managed Identity
+        const credential = new DefaultAzureCredential();
+        const secretClient = new SecretClient(`https://${keyVaultName}.vault.azure.net`, credential);
+        const apiKey = await secretClient.getSecret(secretName);
 
-    //process the API response as needed
-    const searchData = response.data;
+        if (!secret || !secret.value) {
+            throw new Error('Failed to retrieve API key from Azure Key Vault.');
+        }
 
-    //respond with the search data in JSON format
-    context.res = {
-      status: 200,
-      body: searchData,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-  } catch (error) {
-    //handle errors
-    context.log.error('Error:', error.message);
-    context.res = {
-      status: error.response ? error.response.status : 500,
-      body: { error: 'Internal Server Error' },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-  }
+        //use the retrieved Bing API key
+        const query = req.query.q || '';
+        const response = await axios.get('https://api.bing.microsoft.com/bing/v7.0/videos/search', {
+            params: { q: query },
+            headers: { 'Ocp-Apim-Subscription-Key': apiKey.value },
+        });
+
+        //process the API response as needed
+        const searchData = response.data;
+
+        //respond with the search data in JSON format
+        context.res = {
+            status: 200,
+            body: searchData,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        //handle errors
+        context.log.error('Error:', error.message);
+        context.res = {
+            status: error.response ? error.response.status : 500,
+            body: { error: 'Internal Server Error' },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    }
 };
