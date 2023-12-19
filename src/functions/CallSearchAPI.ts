f92c8e954867137f139b5704880e13fb9d6c3d3e
mateomckee <mateomckee64@gmail.com>
2023-12-19 14:07:09 -0600

    Fixed return JSON issue

--- src/functions/CallSearchAPI.ts
+++ src/functions/CallSearchAPI.ts
@@ -3,9 +3,19 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function CallSearchAPI(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
-    return { body: `Hello, ${name}!` };
-};
+    const name = request.query.get('name') || (await request.text()) || 'world';
+
+    const response = {
+        message: `Hello, ${name}!`
+    };
+
+    return {
+        body: JSON.stringify(response),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+}
 
 app.http('CallSearchAPI', {
     methods: ['GET', 'POST'],
