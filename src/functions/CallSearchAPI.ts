cfbcf8d130a1bd384b605ad67d5f3130d29b534a
mateomckee <mateomckee64@gmail.com>
2023-12-19 18:53:19 -0600

    Fix deployment issue

--- src/functions/CallSearchAPI.ts
+++ src/functions/CallSearchAPI.ts
@@ -64,7 +64,7 @@ export async function CallSearchAPI(request: HttpRequest, context: InvocationCon
 }
 
 app.http('CallSearchAPI', {
-    methods: ['GET', 'POST'],
+    methods: ['GET'],
     authLevel: 'anonymous',
     handler: CallSearchAPI
 });
